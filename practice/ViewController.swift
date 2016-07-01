0842f6a3b0c78a00882f09b547bfca076301adaf
Ravikiranios <Ravikiran4ios@Gmail.com>
2016-07-01 12:29:56 +0530

    Update ViewController.swift
    hi i just write  code in git fro pull

--- practice/ViewController.swift
+++ practice/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //hi thi is first
+        //this is from git
     }
 
     override func didReceiveMemoryWarning() {
